773adebdbfc07a153c93dd8d18a439c9d8bf679d
Abishek Kandan <abishekkandan05@gmail.com>
2026-05-28 17:16:42 +0530

    Configured Firebase env variable

--- services/firebase_service.py
+++ services/firebase_service.py
@@ -1,7 +1,15 @@
+import os
+import json
 import firebase_admin
-from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("firebase_key.json")
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+firebase_key = json.loads(
+    os.environ["FIREBASE_KEY"]
+)
+
+cred = credentials.Certificate(firebase_key)
 
 firebase_admin.initialize_app(cred)
 
